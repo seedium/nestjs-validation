71925ee298b650c9c59a3e26aa9a532a5564e37a
Kostya Zgara <k.zgara@seedium.io>
2024-02-29 16:22:27 +0200

    fix: expose original data publicly in validation exception

--- lib/exceptions/validation.exception.ts
+++ lib/exceptions/validation.exception.ts
@@ -2,11 +2,13 @@ import { ErrorObject } from 'ajv';
 
 export class ValidationException extends Error {
   public errors: ErrorObject[];
+  public originalData: unknown;
   public name = 'ValidationException';
 
   constructor(errors: ErrorObject[], originalData?: unknown, message?: string) {
     super(message || `Validation is failed`);
 
     this.errors = errors;
+    this.originalData = originalData;
   }
 }
